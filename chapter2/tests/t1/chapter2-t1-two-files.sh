50d73a4a3ff5d16d78e4214f4c396e2048d12f12
Maxim Omelchenko <roskomtest@mail.ru>
2023-03-29 20:21:57 +0300

    add_funcs_and_others

--- chapter2/tests/t1/chapter2-t1-two-files.sh
+++ chapter2/tests/t1/chapter2-t1-two-files.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
-if ! [[ -r `pwd` ]]; then
+set -eo pipefail
+# Args check
+if [[ "$#" -ne 2 ]]; then
+    echo "Error: Two arguments are required"
+    exit 1
+fi
+# Function for delimiter
+print_separator() {
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+}
+# Function to generate a random string and write to a file with permissions
+gen_rand_str() {
+    local file="$1"
+    openssl rand -base64 20 > "$file" && chmod 700 "$file"
+}
+
+if   [[ ! -r `pwd` ]]; then
     echo "Directory is not readable."
     exit 255
-elif [[ "$#" -lt 2 ]]; then
-    echo "At least 2 args are expected"
-    exit 1
-elif [[ "$#" -gt 2 ]]; then
-    echo "Too many args, only 2 args are expected"
-    exit 1
-elif [[ -e $1 && -f $2 ]]; then
-    cat $1 $2
+elif [[ -e $1 && -e $2 ]]; then
+    print_separator
+    echo file $1 is here: && cat $1
+    print_separator
+    echo file $2 is here: && cat $2
+    print_separator
 elif [[ ! -e $1 && -e $2 ]]; then
     echo file $1 not exist, it will be created. File $2 here:
+    print_separator
     cat $2
-    `openssl rand -base64 20 > $1 && chmod 700 $1`
+    print_separator
+    gen_rand_str $1
 elif [[ ! -e $2 && -e $1 ]]; then
     echo file $2 not exist, it will be created. File $1 here:
+    print_separator
     cat $1
-    `openssl rand -base64 20 > $2 && chmod 700 $2`
+    print_separator
+    gen_rand_str $2
 else echo files dont exist, they will be created. 
-    `openssl rand -base64 20 > $1 && chmod 700 $1; openssl rand -base64 20 > $2 && chmod 700 $2` 
+    gen_rand_str $1; gen_rand_str $2
 fi
