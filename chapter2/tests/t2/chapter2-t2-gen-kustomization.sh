7d489c02a3384b9c0918bb26e730df8d308fc793
Maxim Omelchenko <roskomtest@mail.ru>
2023-03-30 13:47:53 +0300

    Task_2_fix

---
+++ chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# Task 2 - Generate kustomization.yaml
+
+set -eo pipefail
+# Checks (perms, args, dir-exist)
+if ! [[ -r $(pwd) ]]; then
+  echo 'Directory is not readable.'
+  exit 255
+fi
+if [[ "$#" -lt 1 ]]; then
+  echo 'At least 1 or more args are expected'
+  exit 1
+fi
+if [[ ! -d "repos" ]]; then
+  mkdir repos
+fi
+# ssh-keys generation
+for arg in $@; do
+  ssh-keygen -t ed25519 -N "" -C "$arg" -f ./repos/$arg-deploy-key.pem;
+done
+# Head generation
+echo "
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+
+generatorOptions:
+  disableNameSuffixHash: true
+
+secretGenerator:" > ./repos/kustomization.yaml
+# Each rep generation
+for arg in $@; do
+    echo "
+  - name: $arg
+    namespace: argo-cd
+    options:
+      labels:
+        argocd.argoproj.io/secret-type: $arg
+    literals:
+      - name=$arg
+      - url=git@github.com:saritasa-nest/$arg.git
+      - type=git
+      - project=default
+    files:
+      - sshPrivateKey=$arg-deploy-key.pem" >> ./repos/kustomization.yaml
+done
+# kustomize run
+exec kustomize build repos
