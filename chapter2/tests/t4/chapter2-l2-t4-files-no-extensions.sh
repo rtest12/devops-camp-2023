2ca409bca9bdfa0d5cf836255fb74d76d4300d39
Maxim Omelchenko <roskomtest@mail.ru>
2023-04-02 21:29:11 +0300

    arhs_check_fix

--- chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
+++ chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
@@ -4,6 +4,12 @@
 set -eo pipefail
 
 
+# args check
+if [ "$#" -ne 1 ]; then
+  echo "Specify a single directory."
+  exit 1
+fi
+
 # Find only regular files and put in array.
 files=$(find $1 -type f)
 
