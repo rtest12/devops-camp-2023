f9d73989e44c893667559d361687d201164bb26f
Maxim Omelchenko <roskomtest@mail.ru>
2023-04-06 10:57:12 +0300

    gen2

--- chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
+++ chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
@@ -4,35 +4,41 @@
 set -eo pipefail
 
 
-# Error handling function.
+repo_dir=repos
+
+
+# Function for printing error messages and exiting with a specific exit code
+# $1 - The exit code to use when exiting the script
+# $2 - The error message to print
 err() {
-  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+  local code=$1
+  shift
+  echo "[[ERROR]: $(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+  exit "$code"
 }
 
 
-# Checks (perms, args, dir-exist)
-if ! [[ -r $(pwd) ]]; then
-  err "Directory is not readable."
-  exit 255
+# Checking permissions
+if [[ ! -r "$(pwd)" ]]; then
+  err 255 'Directory is not readable.'
 fi
 
 if [[ "$#" -lt 1 ]]; then
-  err "At least 1 or more args are expected"
-  exit 1
+  err 255 'At least 1 or more args are expected'
 fi
 
-if [[ ! -d "repos" ]]; then
-  mkdir repos
+if [[ ! -d "$repo_dir" ]]; then
+  mkdir $repo_dir
 fi
 
 
 # ssh-keys generation
-for arg in $@; do
-  ssh-keygen -t ed25519 -N "" -C "$arg" -f ./repos/$arg-deploy-key.pem;
+for repo_name in "$@"; do
+  rm -f ./$repo_dir/$repo_name-deploy-key.pem* && ssh-keygen -q -t ed25519 -N "" -C "$repo_name" -f ./$repo_dir/$repo_name-deploy-key.pem;
 done
 
 
-# Head generation
+# Generate file beginning
 echo "
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
@@ -40,11 +46,11 @@ kind: Kustomization
 generatorOptions:
   disableNameSuffixHash: true
 
-secretGenerator:" > ./repos/kustomization.yaml
+secretGenerator:" > ./$repo_dir/kustomization.yaml
 
 
-# Each rep generation
-for arg in $@; do
+# Generate each repo content
+for arg in "$@"; do
     echo "
   - name: $arg
     namespace: argo-cd
@@ -57,9 +63,9 @@ for arg in $@; do
       - type=git
       - project=default
     files:
-      - sshPrivateKey=$arg-deploy-key.pem" >> ./repos/kustomization.yaml
+      - sshPrivateKey=$arg-deploy-key.pem" >> ./$repo_dir/kustomization.yaml
 done
 
 
 # kustomize run
-exec kustomize build repos
+exec kustomize build $repo_dir
