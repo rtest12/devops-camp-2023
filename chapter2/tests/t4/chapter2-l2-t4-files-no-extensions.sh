608f90d9e8d07c496215d593dc765e79a6acdc79
Maxim Omelchenko <roskomtest@mail.ru>
2023-04-07 08:10:01 +0300

    gen2

--- chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
+++ chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
@@ -4,24 +4,34 @@
 set -eo pipefail
 
 
-# Error handling function.
+# Function for printing error messages and exiting with a specific exit code
+# $1 - The exit code to use when exiting the script
+# $2 - The error message to print
 err() {
-  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+  local code="$1"
+  shift
+  echo "[[ERROR]: $(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+  exit "$code"
 }
 
 
 # args check
 if [ "$#" -ne 1 ]; then
-  err "Specify a single directory."
-  exit 1
+  err  255 'Specify a single directory.'
+fi
+
+
+# Directory exist check
+if [ ! -d "$1" ]; then
+  err  255 'Directory does not exist.'
 fi
 
 
 # Find only regular files and put in array.
-files=$(find $1 -type f)
+files=( $(find "$1" -type f) )
 
 
 # Then we cut off the paths and extensions.
-for file in $files; do
-  echo ${file%.*}
+for file in "${files[@]}"; do
+  echo "${file%%.*}"
 done
