624e543eabc3576a7b118bfe3c9a7f6af21f71e6
Maxim Omelchenko <roskomtest@mail.ru>
2023-04-07 23:53:59 +0300

    fix

--- chapter2/tests/t5/chapter2-l2-t5-get-file-unique-extensions.sh
+++ chapter2/tests/t5/chapter2-l2-t5-get-file-unique-extensions.sh
@@ -4,26 +4,30 @@
 set -eo pipefail
 
 
-# Error handling function.
+# Function for printing error messages and exiting with a specific exit code
+# $1 - The exit code to use when exiting the script
+# $2 - The error message to print
 err() {
-  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+  local code="$1"
+  shift
+  echo "[[ERROR]: $(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+  exit "$code"
 }
 
 
 # args check
 if [ "$#" -ne 1 ]; then
-  err "Specify a single directory."
-  exit 1
+  err 255 'Specify a single directory.'
 fi
 
 # Find only regular files and put in array
-files=$(find $1 -type f)
+files=$(find "$1" -type f)
 
 
 # Then we cut out the paths and file names, leaving only unique extensions.
-for file in $files; do
+for file in ${files}; do
   list=${file##*/}
-  echo ${list##*.}
-done | sort | uniq
-
-
+  if echo "${list}" | grep -q "\."; then
+    echo "${list##*.}"
+  fi
+done | sort -u
