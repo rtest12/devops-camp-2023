16ba1636bbeafb5d0ba61baf5d3c2510ff232dce
Maxim Omelchenko <roskomtest@mail.ru>
2023-04-08 21:43:38 +0300

    style

--- chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
+++ chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
@@ -19,8 +19,8 @@ err() {
 
 
 # Checking arguments for compliance with repository syntax.
-for i in "$@"; do
-  if [[ ! "${i}" =~ ^[A-Za-z0-9_.-]+$ ]]; then
+for arg in "$@"; do
+  if [[ ! "${arg}" =~ ^[A-Za-z0-9_.-]+$ ]]; then
     err 255 'Invalid repository name.'
   fi
 done
