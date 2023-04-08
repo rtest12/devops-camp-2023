31333b021b688cd02e4b5a1c4f5d79e3ce219361
Maxim Omelchenko <roskomtest@mail.ru>
2023-04-08 13:14:29 +0300

    style

--- chapter2/tests/t8/chapter2-l4-t8-get-tcp-listening-connections.sh
+++ chapter2/tests/t8/chapter2-l4-t8-get-tcp-listening-connections.sh
@@ -5,12 +5,12 @@ set -eo pipefail
 
 
 # set the port range
-port_range_start=10000
-port_range_end=10100
+readonly PORT_RANGE_START='10000'
+readonly PORT_RANGE_END='10100'
 
 
 # port range expr
-port_range="sport >= :$port_range_start and sport <= :$port_range_end"
+port_range="sport >= :$PORT_RANGE_START and sport <= :$PORT_RANGE_END"
 
 
 # Check if there are any TCP connections listening on the specified port range
@@ -18,7 +18,7 @@ while :
 do
   if [[ -z $(ss -tano "$port_range" | grep -v "State") ]]; then
     # If there are no listening connections, print a message and wait for 2 seconds
-    echo "No TCP listening sockets found bound to ports in 10000-10100 range"
+    echo 'No TCP listening sockets found bound to ports in 10000-10100 range'
     sleep 2
   else
     # If there are listening connections, print their details
