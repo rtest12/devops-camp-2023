82057f99237a525e8667ee708fcb744f24f2ff09
Maxim Omelchenko <roskomtest@mail.ru>
2023-04-09 18:32:30 +0300

    gen3

--- chapter2/tests/t3/chapter2-t3-velero-eks-diff.sh
+++ chapter2/tests/t3/chapter2-t3-velero-eks-diff.sh
@@ -4,8 +4,8 @@
 set -eo pipefail
 
 
-readonly SAVED_NS_URL='https://gist.github.com/dmitry-mightydevops/016139747b6cefdc94160607f95ede74/raw/velero.yaml'
-readonly ALL_NS_URL='https://gist.githubusercontent.com/dmitry-mightydevops/297c4e235b61982f21a0bbbf7319ac24/raw/kubernetes-namespaces.txt'
+readonly VALERO_URL='https://gist.github.com/dmitry-mightydevops/016139747b6cefdc94160607f95ede74/raw/velero.yaml'
+readonly ALL_NAMESPACES_URL='https://gist.githubusercontent.com/dmitry-mightydevops/297c4e235b61982f21a0bbbf7319ac24/raw/kubernetes-namespaces.txt'
 
 
 # Function for printing error messages and exiting with a specific exit code
@@ -14,31 +14,42 @@ readonly ALL_NS_URL='https://gist.githubusercontent.com/dmitry-mightydevops/297c
 err() {
   local code="$1"
   shift
-  echo "[[ERROR]: $(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+  echo "[[ERROR]: $(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
   exit "$code"
 }
 
-# File exist check
-if curl -s --head  --request GET "$ALL_NS_URL" | grep "404" > /dev/null; then
-  err 255 'File with saved namespaces list not found'
+
+# Function for checking file existence and downloading it.
+# $1 - URL to the file
+# $2 - Var name to save
+get_file() {
+  local url="$1"
+  local save_to_var="$2"
+  if ! curl -sSL --head --request GET "$url" | grep "200" > /dev/null; then
+    err 255 "Error getting $url"
+  else
+    declare -g "$save_to_var=$(curl -sSL "$url")"
+  fi
+}
+
+
+# Check if yq is installed.
+if ! command -v yq > /dev/null; then
+  err 255 'yq is not installed'
 fi
 
-if curl -s --head  --request GET "$ALL_NS_URL" | grep "404" > /dev/null; then
-  err 255 'File with all namespases list not found'
-fi
+
+get_file "$VALERO_URL" "valero"
+get_file "$ALL_NAMESPACES_URL" "all_namespaces"
 
 
-# Downloading YAML, extracting already saved namespaces into array.
-saved_namespaces=$(curl -sSL $SAVED_NS_URL | yq '.spec.source.helm.values | from_yaml | .schedules.[].template.includedNamespaces[]')
-
-
-# Downloading YAML with all namespaces and saving into array.
-all_namespaces=$(curl -sSL "$ALL_NS_URL")
+# Processing YAML, extracting namespaces being preserved into an array.
+saved_namespaces=$(yq '.spec.source.helm.values | from_yaml | .schedules.[].template.includedNamespaces[]' <<< "$valero")
 
 
 # We output only those namespaces that are not present in the array of saved namespaces.
-for val in ${all_namespaces[@]}; do
-  if [[ ! ${saved_namespaces[@]} =~ ${val} ]]; then
-    echo "$val"
+for namespace in ${all_namespaces[@]}; do
+  if [[ ! "${saved_namespaces[@]}" =~ "${namespace}" ]]; then
+    echo "$namespace"
   fi
 done
