04cf09ac454a4a4f0414e2b831efb2fad2b6e77d
Maxim Omelchenko <roskomtest@mail.ru>
2023-04-28 17:26:43 +0300

    add sleep constant

--- chapter2/tests/t8/chapter2-l4-t8-get-tcp-listening-connections.sh
+++ chapter2/tests/t8/chapter2-l4-t8-get-tcp-listening-connections.sh
@@ -6,7 +6,7 @@ set -eo pipefail
 # set the port range
 readonly PORT_RANGE_START='10000'
 readonly PORT_RANGE_END='10100'
-
+readonly SLEEP_SECONDS='1'
 
 # port range expr
 port_range="sport >= :$PORT_RANGE_START and sport <= :$PORT_RANGE_END"
@@ -18,11 +18,11 @@ do
   if [[ -z $(ss -l "${port_range}" | grep -v "State") ]]; then
     # If there are no listening connections, print a message and wait for 2 seconds
     echo 'No TCP listening sockets found bound to ports in 10000-10100 range'
-    sleep 2
+    sleep "${SLEEP_SECONDS}"
   else
     # If there are listening connections, print their details
     ss -tan "${port_range}" | awk '{printf "%-20s %-20s\n", $4, $5}'
-    sleep 1
+    sleep "${SLEEP_SECONDS}"
     clear
   fi
 done
