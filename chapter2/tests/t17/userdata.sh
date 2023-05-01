216c1dc44ffd47a61ad2fa18dd85dc660d4ea51e
Maxim Omelchenko <roskomtest@mail.ru>
2023-05-01 03:15:07 +0300

    minnor style fix

--- chapter2/tests/t17/userdata.sh
+++ chapter2/tests/t17/userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# userdata task 17
+# Userdata task 17
 
 # Install SSM agent
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
