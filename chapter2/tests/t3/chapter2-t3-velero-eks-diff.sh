014897b9b28f797b84cf0b01143f232492f36ee9
Maxim Omelchenko <roskomtest@mail.ru>
2023-05-11 21:36:36 +0300

    New version, add comm

--- chapter2/tests/t3/chapter2-t3-velero-eks-diff.sh
+++ chapter2/tests/t3/chapter2-t3-velero-eks-diff.sh
@@ -2,7 +2,6 @@
 # Task 3 - find namespaces without the backup
 set -eo pipefail
 
-
 readonly VELERO_URL='https://gist.github.com/dmitry-mightydevops/016139747b6cefdc94160607f95ede74/raw/velero.yaml'
 readonly ALL_NAMESPACES_URL='https://gist.githubusercontent.com/dmitry-mightydevops/297c4e235b61982f21a0bbbf7319ac24/raw/kubernetes-namespaces.txt'
 
@@ -37,16 +36,9 @@ fi
 backup_namespaces=$(get_file "${VELERO_URL}")
 all_namespaces=$(get_file "${ALL_NAMESPACES_URL}")
 
-# Convert the output to an array.
-#readarray -t backup_namespaces_array <<< "$backup_namespaces"
-
-# Process the backup namespaces YAML, and extract the included namespaces.
-saved_namespaces=$(yq '.spec.source.helm.values | from_yaml | .schedules.[].template.includedNamespaces[]' <<< "${backup_namespaces}")
-
+# Extract the included namespaces from yaml.
+query='.spec.source.helm.values | from_yaml | .schedules.[].template.includedNamespaces[]'
+saved_namespaces=$(yq "$query" <<< "${backup_namespaces}")
 
 # We output only those namespaces that are not present in the array of saved namespaces.
-for namespace in ${all_namespaces[@]}; do
-  if [[ ! "${saved_namespaces[@]}" =~ "${namespace}" ]]; then
-    echo "${namespace}"
-  fi
-done | sort -u
+comm -23 <(echo "${all_namespaces}" | sort -u) <(echo "${saved_namespaces}" | sort -u)
