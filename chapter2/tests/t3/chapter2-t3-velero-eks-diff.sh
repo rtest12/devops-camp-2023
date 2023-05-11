119d71b6ce48c3decb465bba4105458fa68926f5
Maxim Omelchenko <roskomtest@mail.ru>
2023-05-11 08:56:29 +0300

    del file_1

--- chapter2/tests/t3/chapter2-t3-velero-eks-diff.sh
+++ chapter2/tests/t3/chapter2-t3-velero-eks-diff.sh
@@ -6,7 +6,6 @@ set -eo pipefail
 readonly VELERO_URL='https://gist.github.com/dmitry-mightydevops/016139747b6cefdc94160607f95ede74/raw/velero.yaml'
 readonly ALL_NAMESPACES_URL='https://gist.githubusercontent.com/dmitry-mightydevops/297c4e235b61982f21a0bbbf7319ac24/raw/kubernetes-namespaces.txt'
 
-
 # Function for printing error messages and exiting with a specific exit code
 # $1 - The exit code to use when exiting the script
 # $2 - The error message to print
@@ -17,20 +16,16 @@ err() {
   exit "${code}"
 }
 
-
 # Function for checking file existence and downloading it.
 # $1 - URL to the file
-# $2 - Var name to save
 get_file() {
   local url="$1"
-  local save_to_var="$2"
   if ! curl -sSL --head --request GET "${url}" | grep "HTTP/2\ 200" > /dev/null; then
     err 255 "Error getting ${url}"
   else
-    declare -g "$save_to_var=$(curl -sSL "${url}")"
+    curl -sSL "${url}"
   fi
 }
-
 
 # Check if yq is installed.
 if ! command -v yq > /dev/null; then
@@ -38,17 +33,20 @@ if ! command -v yq > /dev/null; then
 fi
 
 
-get_file "${VELERO_URL}" "VELERO"
-get_file "${ALL_NAMESPACES_URL}" "all_namespaces"
+# Download from URLs using the get_file function.
+backup_namespaces=$(get_file "${VELERO_URL}")
+all_namespaces=$(get_file "${ALL_NAMESPACES_URL}")
 
+# Convert the output to an array.
+readarray -t backup_namespaces_array <<< "$backup_namespaces"
 
-# Processing YAML, extracting namespaces being preserved into an array.
-saved_namespaces=$(yq '.spec.source.helm.values | from_yaml | .schedules.[].template.includedNamespaces[]' <<< "${VELERO}")
+# Process the backup namespaces YAML, and extract the included namespaces.
+saved_namespaces=$(yq '.schedules.[].template.includedNamespaces[]' <<< "${backup_namespaces_array}")
 
 
 # We output only those namespaces that are not present in the array of saved namespaces.
 for namespace in ${all_namespaces[@]}; do
-  if [[ ! "${saved_namespaces[@]}" =~ "${namespace}" ]]; then
+  if [[ ! "${backup_namespaces[@]}" =~ "${namespace}" ]]; then
     echo "${namespace}"
   fi
-done
+done | sort -u
